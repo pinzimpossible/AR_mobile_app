93c5acf5aef156958cf49d041acc3e661c0c7b7a
Toai Huynh Thanh <toaiht@DFM.INTRANET>
2019-09-05 11:56:00 +0700

    Delete nav

--- AMA/navigation/MainTabNavigator.js
+++ AMA/navigation/MainTabNavigator.js
@@ -1,42 +1,47 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import { createStackNavigator, createBottomTabNavigator} from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 
-const config = Platform.select({
-  web: { headerMode: 'screen' },
-  default: {},
-});
+// const config = Platform.select({
+//   web: { headerMode: 'screen' },
+//   default: {},
+// });
 
-const HomeStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-  },
-  config
+// const HomeStack = createStackNavigator(
+//   {
+//     Home: HomeScreen,
+//   },
+//   config
+// );
+
+// HomeStack.navigationOptions = {
+//   tabBarLabel: 'Home',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={
+//         Platform.OS === 'ios'
+//           ? `ios-information-circle${focused ? '' : '-outline'}`
+//           : 'md-information-circle'
+//       }
+//     />
+//   ),
+// };
+
+// HomeStack.path = '';
+
+// const tabNavigator = createBottomTabNavigator({
+//   HomeStack
+// });
+
+// tabNavigator.path = '';
+
+// export default tabNavigator;
+
+export default createStackNavigator({
+  Home:{screen: HomeScreen},
+  },{}
 );
-
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
-
-HomeStack.path = '';
-
-const tabNavigator = createBottomTabNavigator({
-  HomeStack
-});
-
-tabNavigator.path = '';
-
-export default tabNavigator;
